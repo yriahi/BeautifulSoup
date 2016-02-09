0bbecd6a6c5717d93c5b4b4c9ad27f7b13dd8b7c
Youssef Riahi <y.riahi@gmail.com>
2016-02-09 15:54:58 -0500

    bug fix: non cms pages had content id

--- get_mixed_content_by_folder.py
+++ get_mixed_content_by_folder.py
@@ -1,33 +1,24 @@
 #----------------------------------------------------------------#
-# File name: BeautifulSoup_Get_Mixed_Content_by_Directory.py
+# File name: get_mixed_content_by_folder.py
 # Author: Youssef Riahi
 # Date created: 12/03/2015
-# Date last modified: 12/09/2015
-# Script version: 1.2
+# Date last modified: 02/09/2016
+# Script version: 2.0
 # Python Version: 2.7.10
 #----------------------------------------------------------------
-# 1.0 - Initial version parsing webages for hot images, css and js
-#     - Results are printed to screen or sent to file via shell.
-# 1.1 - Fix to close each page after parsing is done.
-#     - Added content id.
-#     - Added exluded files list.
-# 1.2 - Removed htm or html as usage arguments.
-#     - Added list of web file extensions (.html, .html) for 
-#       flexibility. Script can also scan asp, aspx...etc
-#     - Changed output file format to CSV.
-#     - Added: if __name__ == '__main__'   See comment at bottom.
+# Description: 
+#    The script will walk a directory structure of a website and
+#    scans the html code for "Mixed Content" (non-https items)
+#    It looks for the following if the they start with 'http://':
+#    - Linked IMAGES: <img src>
+#    - Linked CSS files: <link rel>
+#    - Linked SCRIPTS: <script src>
+# 
+# Usage:
+#    python get_mixed_content_by_website_folder /var/www/
+# 
+# Notes:
 #----------------------------------------------------------------#
-'''
-Description: 
-   A python script that finds the hot linked items (https and https): Images, CSS and JS.
-   The script can be expanded to find other HTML elements, tags, attributes...etc
-
-Usage: 
-The script accepts 2 args: 
-   1- the target folder. 
-   2- File extension (specify: html/html).
-   python Get_Soup_Items_and_Pages.py.py /var/www/
-'''
 
 # load the required modules
 import os, sys, bs4, re
@@ -36,85 +27,166 @@ from sys import argv
 from collections import Counter
 from bs4 import BeautifulSoup
 
-# list web page types to be parsed
-file_extensions = ['.htm', '.html']
-
-# exclude files here; in case you are using include files
-exclude_mgs_files = [
-                     'mgsSnFooterLinks.html',
-                     'mgsInMegadd.html',
-                     'mgsInNavBreadcrumbs.html',
-                     'mgsInNavAlertStatus.html',
-                     'mgsInNavAlert.html',
-                     'navon_includes.html'
-                     ]
-
-# setting up the arguments:
-# the script takes 1 arg; and that is 'yourpath' being scanned for 
+# setting up target folder as script argument
 script, yourpath = argv
 
-# this is a (dictionary) of all pages, it is empty now.
-list_of_pages = {}
+# folder from which this script is executed
+Script_Home = os.getcwd()
 
-# creating a new file to which we can save the list of files
-list_of_pages_file = open('list_of_pages.csv', 'wb')
-list_of_pages_file.write('Hotlink item, Page, Content ID' + os.linesep)
+# folder where results/reports are generated 
+Output_Directory_Name = 'mixed_content_output'
+Output_Directory_Path = os.path.join(Script_Home, Output_Directory_Name)
 
-# function to search directory recursively; and parse htm/html files.
+# check if the folder does not exist
+if not os.path.isdir(Output_Directory_Name):
+   print 'Creating output directory...'
+   os.mkdir(Output_Directory_Name)
+
+# list web page extensions to be parsed
+file_extensions = ['.htm', '.html']
+
+# regex for urls starting with...
+URL_REGEX = 'http://.+'
+
+# exclude files
+exclude_mgs_files = ['file_01.html', 'file_02.html']
+
+# exclude folders
+Sub_Folders_List_Exclude = ['folder_a', 'folder_b']
+
+# create and empty list of subfolders in 'yourpath'
+Sub_Folders_List = []
+
+# create and empty list of subfolders in 'yourpath'
+Sub_Folders_List_Names = []
+
+# create an empty dictionary with site names and matching path
+Sub_Folders_Dictionary = {}
+
+# keep track of the files that we are going to parse
+files_2_parse_list = []
+
+# create one output file if needed
+all_mixed_content_pages = open('all_mixed_content_pages.csv','wb')
+all_mixed_content_pages.write('Hotlink item, Page, Content ID' + os.linesep)
+
+def get_sub_folders():
+   for Sub_Folder in os.listdir(str(yourpath)):
+      # get folders only
+      if os.path.isdir(os.path.join(str(yourpath), Sub_Folder)):
+         # skip symlinks
+         if os.path.islink(os.path.join(str(yourpath), Sub_Folder)) == False:
+            Sub_Folder_Name = os.path.basename(os.path.join(str(yourpath), Sub_Folder))
+            # check exclude list
+            if Sub_Folder not in Sub_Folders_List_Exclude:
+               # save folder name only to a list
+               Sub_Folders_List_Names.append(os.path.basename(os.path.join(str(yourpath), Sub_Folder)))
+               # add folder path to a list
+               Sub_Folders_List.append(os.path.join(str(yourpath), Sub_Folder))
+               # create 'Sub_Folders_Dictionary' with key(foleder name) and value(path)
+               Sub_Folders_Dictionary[os.path.basename(os.path.join(str(yourpath), Sub_Folder))] = str(os.path.join(str(yourpath), Sub_Folder))
+
+# search directory recursively; and parse htm/html files.
 def BeautifulSoup_Get_Mixed_Content_by_Directory():
-   for root, dirs, files in os.walk(str(yourpath)):
-      for f in files:
-         #if yourext == os.path.splitext(f)[1][1:]:  # old version
-         file_extension = os.path.splitext(os.path.join(root, f))[1]
-         # webpage extensions being parsed. the list can be expanded.
-         if file_extension in file_extensions:
-            if str(f) not in exclude_mgs_files:               
-               one_page = os.path.join(root,f)
-               fp = open(one_page,'rb')
-               soup = BeautifulSoup(fp.read(), 'html.parser')
-               
-               # Rhythmyx CMS content_id
-               # <meta name="contentID" content="1536015"  />
-               # not all the *.html files have 'meta' information, so we try first!
-               try:
-                  content_id = soup.find('meta', {'name':'contentID'})['content']
-               except:
-                  # if 'one_page' does not have it...keep going!
-                  pass
+   for Sub_Folder in Sub_Folders_List_Names:
+      # show current folder being worked on...
+      print '[+] Processing %s folder...' % (Sub_Folder)
+      
+      # create an output csv file with a header
+      output_file_format = Sub_Folder + '_mixed_content_pages.csv'
+      list_of_pages_file = open(os.path.join(Output_Directory_Path, output_file_format), 'wb')
+      list_of_pages_file.write('Hotlink item, Page, Content ID' + os.linesep)
 
-               # IMAGES: match this pattern example:
-               # <img src="http://notmysite.com/image.jpg" height="350" width="200">
-               img_urls = soup.find_all('img', attrs={'src': re.compile('^http(s)?://')})
-               for img_url in img_urls:
-                  clean_img_url = img_url['src']
-                  list_of_pages_file.write('%s, %s, %s' % (clean_img_url, one_page, content_id) + os.linesep)
-               
-               # CSS: match this pattern example: href that starts with http ot https
-               # <link rel="stylesheet" href="/resources/styles/main.css" />
-               css_urls = soup.find_all('link', attrs={'href': re.compile('^http(s)?://')})
-               for css_url in css_urls:
-                  clean_css_url = css_url['href']
-               if clean_css_url.endswith('.css'): # get css only, we dont want schema.dcterms 
-                  list_of_pages_file.write('%s, %s, %s' % (clean_css_url, one_page, content_id) + os.linesep)
+      #for root, dirs, files in os.walk(os.path.join(root,Sub_Folder)):
+      #   print os.path.join(root,f)
 
-               # JS: match this pattern: src that starts with http or https
-               # <script src="http://notmyjs.com/resources/scripts/vendor/jquery.js"></script>
-               js_urls = soup.find_all('script', attrs={'src': re.compile('^http(s)?://')})
-               for js_url in js_urls:
-                  clean_js_url = js_url['src']
-                  if clean_js_url.endswith('.js'): # get urls ending with .js only 
-                     list_of_pages_file.write('%s, %s, %s' % (clean_js_url, one_page, content_id) + os.linesep)
-              
-               # close the opened html/htm file being read 'one_page'.
-               fp.close()
+      for root, dirs, files in os.walk(str(yourpath) +'/'+ str(Sub_Folder)):
+         #print os.path.abspath(yourpath)
+         for f in files:
+            # get the file extension in 'Sub_Folder'
+            file_extension = os.path.splitext(os.path.join(root, f))[1]
+            
+            # if extension matches...'htm or html'
+            if file_extension in file_extensions:
+               # if file is not in exclude list (to avoid apache html includes)
+               if str(f) not in exclude_mgs_files:               
+                  # build path to each page
+                  one_page = os.path.join(root,f)
+                  files_2_parse_list.append(one_page)
+                  
+                  try:
+                     # open each web page in 'Read Binary' mode
+                     fp = open(one_page,'rb')
+                     
+                     # feed the opened htm/html file to BeautifulSoup parser
+                     soup = BeautifulSoup(fp.read(), 'html.parser')
+                     
+                     # Rhythmyx CMS content_id:    <meta name="contentID" content="1536015"  />
+                     # not all the htm(l) files have 'meta' information, so we try first!
+                     content_id_meta = soup.find('meta', {'name':'contentID'})
+                     if content_id_meta is None:
+                        # IMAGES:   <img src="http://notmysite.com/image.jpg" height="350" width="200">
+                        img_urls = soup.find_all('img', attrs={'src': re.compile(URL_REGEX)})
+                        for img_url in img_urls:
+                           clean_img_url = img_url['src']
+                           list_of_pages_file.write('%s, %s' % (clean_img_url, one_page) + os.linesep)
+                           all_mixed_content_pages.write('%s, %s' % (clean_img_url, one_page) + os.linesep)
+                     
+                        # CSS:   <link rel="stylesheet" href="/resources/styles/main.css" />
+                        css_urls = soup.find_all('link', attrs={'href': re.compile(URL_REGEX)})
+                        for css_url in css_urls:
+                           clean_css_url = css_url['href']
+                        if clean_css_url.endswith('.css'): # get css only, we dont want schema.dcterms 
+                           list_of_pages_file.write('%s, %s' % (clean_css_url, one_page) + os.linesep)
+                           all_mixed_content_pages.write('%s, %s' % (clean_css_url, one_page) + os.linesep)
+
+                        # JS:   <script src="http://notmyjs.com/resources/scripts/vendor/jquery.js"></script>
+                        js_urls = soup.find_all('script', attrs={'src': re.compile(URL_REGEX)})
+                        for js_url in js_urls:
+                           clean_js_url = js_url['src']
+                           if clean_js_url.endswith('.js'): # get urls ending with .js only 
+                              list_of_pages_file.write('%s, %s' % (clean_js_url, one_page) + os.linesep)
+
+                     else:
+                        # get just the content id
+                        content_id = soup.find('meta', {'name':'contentID'})['content']
+
+                        # IMAGES:   <img src="http://notmysite.com/image.jpg" height="350" width="200">
+                        img_urls = soup.find_all('img', attrs={'src': re.compile(URL_REGEX)})
+                        for img_url in img_urls:
+                           clean_img_url = img_url['src']
+                           list_of_pages_file.write('%s, %s, %s' % (clean_img_url, one_page, content_id) + os.linesep)
+                           all_mixed_content_pages.write('%s, %s, %s' % (clean_img_url, one_page, content_id) + os.linesep)
+                     
+                        # CSS:   <link rel="stylesheet" href="/resources/styles/main.css" />
+                        css_urls = soup.find_all('link', attrs={'href': re.compile(URL_REGEX)})
+                        for css_url in css_urls:
+                           clean_css_url = css_url['href']
+                        if clean_css_url.endswith('.css'): # get css only, we dont want schema.dcterms 
+                           list_of_pages_file.write('%s, %s, %s' % (clean_css_url, one_page, content_id) + os.linesep)
+                           all_mixed_content_pages.write('%s, %s, %s' % (clean_css_url, one_page, content_id) + os.linesep)
+
+                        # JS:   <script src="http://notmyjs.com/resources/scripts/vendor/jquery.js"></script>
+                        js_urls = soup.find_all('script', attrs={'src': re.compile(URL_REGEX)})
+                        for js_url in js_urls:
+                           clean_js_url = js_url['src']
+                           if clean_js_url.endswith('.js'): # get urls ending with .js only 
+                              list_of_pages_file.write('%s, %s, %s' % (clean_js_url, one_page, content_id) + os.linesep)
+                     
+                     # close htm/html file after reading and parsing.
+                     fp.close()
+                  
+                  except IOError as e:
+                     print "Unable to open '%s' file" % (one_page)
+      
+      # close the file after writing the matching results
+      list_of_pages_file.close()
 
 # run the code in this file only if it is called directly by python.
 # a condition can be placed to allow/prevent other script to embded this file.
 # details about this can be found here: http://ibiblio.org/g2swap/byteofpython/read/module-name.html
 if __name__ == '__main__':
    # run the big function above
+   get_sub_folders()
    BeautifulSoup_Get_Mixed_Content_by_Directory()
-   # close the file after writing the matching results
-   list_of_pages_file.close()
-
 
